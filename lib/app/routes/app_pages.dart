1f9263418ff708787693dc57705ee930cf0b5b41
muslimah27 <muslimah2703@gmail.com>
2022-06-28 14:29:06 +0700

    Tugas AppTaskManagement

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,41 @@
+import 'package:get/get.dart';
+
+import '../modules/about/bindings/about_binding.dart';
+import '../modules/about/views/about_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+import '../modules/name/bindings/name_binding.dart';
+import '../modules/name/views/name_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.NAME,
+      page: () => const NameView(),
+      binding: NameBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.ABOUT,
+      page: () => const AboutView(),
+      binding: AboutBinding(),
+    ),
+  ];
+}
