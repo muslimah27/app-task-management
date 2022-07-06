2c165794f10fe2fd42a409ad452c89c4549e0d4b
muslimah <102421885+muslimah27@users.noreply.github.com>
2022-07-06 20:26:47 +0700

    appcolors vid6

--- lib/app/utils/style/appColors.dart
+++ lib/app/utils/style/appColors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class appColors{
-  static const primaryBg = Color(0xFFAFD7F8);
+  static const cardBg = Color(0xFFD8CDC8);
+  static const primaryBg = Color(0xFFB1D5F1);
   static const primaryText = Color(0xFF797676);
 }
