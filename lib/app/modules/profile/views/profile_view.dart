7a702d0b23c410988c23aac10501d826716b19ff
muslimah <102421885+muslimah27@users.noreply.github.com>
2022-07-07 02:06:48 +0700

    profil view vid8

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,3 +1,5 @@
+import 'package:app_task_management/app/utils/widget/MyTask.dart';
+import 'package:app_task_management/app/utils/widget/ProfilWidget.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -58,40 +60,74 @@ final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
               ],
             ),
             const Spacer(),
-            const Icon(Icons.notifications, 
-            color: appColors.primaryText, 
-            size: 30,
-        ),
-            const SizedBox(
-              width: 15, 
-        ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-            child: const CircleAvatar(
-              backgroundColor: Colors.amber, 
-              radius: 25,
-              foregroundImage: NetworkImage(
-                "https://yt3.ggpht.com/a/AATXAJyUEgqkrx-_Vhz-cpHNMfONR22tfRaCScNzyg=s900-c-k-c0xffffffff-no-rj-mo"),
-                ),
-            )
+            
+            GestureDetector(
+              onTap: () {
+                Get.defaultDialog(
+                  title: 'Log Out',
+                  content: const Text('Apakah yakin akan keluar?'),
+                  cancel: ElevatedButton(
+                    onPressed: () => Get.back(),
+                    child: const Text('Batal'),
+                  ),
+                  confirm: ElevatedButton(
+                      onPressed: () => Get.toNamed(Routes.LOGIN),
+                      child: const Text('Ya')),
+                );
+              },
+              child: Row(
+                children: const [
+                  Text('Log Out',
+                      style: TextStyle(fontSize: 15, color: appColors.primaryText)),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Icon(
+                    Icons.logout,
+                    color: appColors.primaryText,
+                    size: 20,
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
             //content / isipage /screen
                 Expanded(
                   child: Container(
-                  padding: const EdgeInsets.all(50),
-                  margin: !context.isPhone  
-                  ? const EdgeInsets.all(10)
-                  : const EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: !context.isPhone 
-                      ? BorderRadius.circular(50)
-                      : BorderRadius.circular(30),
-
+                    padding: !context.isPhone
+                        ? const EdgeInsets.all(30)
+                        : const EdgeInsets.all(10),
+                    margin:
+                        !context.isPhone ? const EdgeInsets.all(10) : null,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: !context.isPhone
+                        ? BorderRadius.circular(50)
+                        : BorderRadius.circular(30)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                    ProfilWidget(),
+                      Text(
+                          'My Task',
+                            style: TextStyle(
+                            color: appColors.primaryText,
+                            fontSize: 15,
+                           ),
+                        ),
+                      SizedBox(
+                            height: 5,
+                          ),
+                      SizedBox(
+                        height: 200,
+                        child: MyTask(),
                       ),
-                ))
+                    ],),
+                ),
+              )
               ],
             ),
          ),
@@ -100,3 +136,4 @@ final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
     );
   }
 }
+
